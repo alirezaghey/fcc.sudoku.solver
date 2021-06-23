1bd7b2263f210a221c378e9b5eb5883eebf8fcce
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2021-06-23 18:08:57 +0200

    first working version
    passes all functional tests of the online checker
    lacks its own unit and functional tests

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,88 @@
 class SudokuSolver {
-
-  validate(puzzleString) {
+  constructor() {
+    this.checkRowPlacement = this.checkRowPlacement.bind(this);
+    this.checkColPlacement = this.checkColPlacement.bind(this);
+    this.checkRegionPlacement = this.checkRegionPlacement.bind(this);
+    this.solve = this.solve.bind(this);
   }
+  validate(puzzleString) {}
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    // if cell filled by anything but the value itself it's automatic invalid
+    if (puzzleString[row * 9 + column] !== "." && puzzleString[row * 9 + column] != value) {
+      return false;
+    }
+    // starting at column going through all the columns in this row
+    for (let i = row * 9; i < row * 9 + 9; i++) {
+      // we don't check the original cell as it can contain the value itself
+      if (i === row * 9 + column) continue;
+      // if value already present, it's not valid
+      if (puzzleString[i] == value) return false;
+    }
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    // if cell filled by anything but the value itself it's an automatic invalid
+    if (puzzleString[row * 9 + column] !== "." && puzzleString[row * 9 + column] != value) {
+      return false;
+    }
+    // starting at row zero we add rows in each iteration to sweep the whole column
+    for (let i = column; i < 81; i += 9) {
+      // we don't check the original cell as it can contain the value itself
+      if (i === row * 9 + column) continue;
+      // if value already present, it's not valid
+      if (puzzleString[i] == value) return false;
+    }
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    // if cell filled by anything but the value itself it's an automatic invalid
+    if (puzzleString[row * 9 + column] !== "." && puzzleString[row * 9 + column] != value) {
+      console.log("hell");
+      return false;
+    }
+    const [oRow, oCol] = [row, column];
+    row = Math.floor(row / 3) * 3;
+    column = Math.floor(column / 3) * 3;
+    for (let i = row; i < row + 3; i++) {
+      for (let j = column; j < column + 3; j++) {
+        // we don't check the original cell as it can contain the value itself
+        if (i * 9 + j === oRow * 9 + oCol) continue;
+        // if value already present, it's not valid
+        if (puzzleString[i * 9 + j] == value) return false;
+      }
+    }
+    return true;
   }
 
   solve(puzzleString) {
-    
+    const backtrack = (idx, grid) => {
+      if (idx >= 81) return grid;
+
+      // for (let i = idx; i < 81; i++) {
+      const [row, col] = [Math.floor(idx / 9), idx % 9];
+      for (let k = 1; k <= 9; k++) {
+        if (
+          this.checkRowPlacement(grid, row, col, k) &&
+          this.checkColPlacement(grid, row, col, k) &&
+          this.checkRegionPlacement(grid, row, col, k)
+        ) {
+          // console.log(grid);
+          const res = backtrack(idx + 1, grid.substr(0, idx) + String(k) + grid.substr(idx + 1));
+          // console.log(res);
+          if (typeof res !== "boolean") return res;
+          // }
+        }
+      }
+      return false;
+    };
+    // console.log(puzzleString);
+    const res = backtrack(0, puzzleString);
+    // if (res) return res;
+    return res;
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,78 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
+const ROW_TO_NUM = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8 };
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    if (
+      req.body.puzzle === undefined ||
+      req.body.coordinate === undefined ||
+      req.body.value === undefined
+    ) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+    const puzzle = req.body.puzzle;
+    const [row, col] = [req.body.coordinate[0], req.body.coordinate[1]];
+    const value = req.body.value;
+    if (
+      row.toLowerCase().charCodeAt(0) < "a".charCodeAt(0) ||
+      row.toLowerCase().charCodeAt(0) > "i".charCodeAt(0) ||
+      Number.isNaN(Number(col)) ||
+      Number(col) < 1 ||
+      Number(col) > 9
+    ) {
+      return res.json({ error: "Invalid coordinate" });
+    }
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    if (Number.isNaN(Number(value)) || Number(value) < 1 || Number(value) > 9) {
+      return res.json({ error: "Invalid value" });
+    }
+    if (invalidChars(puzzle)) {
+      return res.json({ error: "Invalid characters in puzzle" });
+    }
 
-    });
+    if (puzzle.length !== 81) {
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
+
+    const rowStat = solver.checkRowPlacement(puzzle, ROW_TO_NUM[row], col - 1, value);
+    const colStat = solver.checkColPlacement(puzzle, ROW_TO_NUM[row], col - 1, value);
+    const regionStat = solver.checkRegionPlacement(puzzle, ROW_TO_NUM[row], col - 1, value);
+
+    if (!rowStat || !colStat || !regionStat) {
+      const conflict = [];
+      if (!rowStat) conflict.push("row");
+      if (!colStat) conflict.push("column");
+      if (!regionStat) conflict.push("region");
+      return res.json({ valid: false, conflict });
+    } else {
+      return res.json({ valid: true });
+    }
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    const puzzle = req.body.puzzle;
+    if (!puzzle) {
+      return res.json({ error: "Required field missing" });
+    }
+    if (invalidChars(puzzle)) {
+      return res.json({ error: "Invalid characters in puzzle" });
+    }
+    if (puzzle.length !== 81) {
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
+    const result = solver.solve(puzzle);
+    if (!result) {
+      return res.json({ error: "Puzzle cannot be solved" });
+    }
+    return res.json({ solution: result });
+  });
+};
+
+const invalidChars = (puzzle) => {
+  if (/[^1-9\.]+/.test(puzzle)) return true;
+  return false;
 };
